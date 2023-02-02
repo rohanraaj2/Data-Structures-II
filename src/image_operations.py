8b4e683810cdcb4d0641cf819a206bb64f586fdd
Rohan Raj <rohan.raaj2@gmail.com>
2023-02-03 02:39:20 +0500

    rotation done

--- src/image_operations.py
+++ src/image_operations.py
@@ -30,7 +30,35 @@ def rotations(src: MyImage) -> MyImage:
     Returns:
     an image twice the size of src and containing the 4 rotations of src.
     """
-    pass
+    original_width = src.size[0]
+    original_height = src.size[1]
+
+    new_width = original_width * 2
+    new_height = original_height * 2
+
+    resulting_image = MyImage((new_width, new_height))
+
+    for row in range(original_width):
+        for column in range(original_height):
+            # looping over each pixel to set rgb values
+
+            rgb_value = src.get(row, column)
+
+            # 1st image (rotated 90 degrees anticlockwise) on 1st row, 1st column
+            resulting_image.set(original_height - 1 - column, row, rgb_value)
+
+            # 2nd image (original / unrotated) on 1st row, 2nd column
+            resulting_image.set(row, column + original_width, rgb_value)
+
+            # 3rd image (upside down / rotated 180 degrees) on 2nd row, 1st column
+            resulting_image.set(original_width - 1 - row + original_height,
+                                original_height - 1 - column, rgb_value)
+
+            # 4th image (rotated 270 degrees anticlockwise / 90 degrees clockwise) on 2nd row, 2nd column
+            resulting_image.set(column + original_height,
+                                original_height - 1 - row + original_width, rgb_value)
+
+    return resulting_image
 
 
 def apply_mask(src: MyImage, maskfile: str, average: bool = True) -> MyImage:
@@ -50,6 +78,7 @@ def apply_mask(src: MyImage, maskfile: str, average: bool = True) -> MyImage:
     an image which the result of applying the specified mask to src.
     """
     pass
+
 
 def resize(src: MyImage) -> MyImage:
     """Returns an image which has twice the dimensions of src.
