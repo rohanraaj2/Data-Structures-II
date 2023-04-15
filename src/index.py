3a778d1782ae1899db6c79d5915ba1cec679690b
ayilaemad <ae07352@st.habib.edu.pk>
2023-04-15 16:06:25 +0500

    Update index.py

--- src/index.py
+++ src/index.py
@@ -1,4 +1,15 @@
 import math
 from document import Document
+
 class InvertedIndex:
-    pass
+    def query(self, terms:str, k: int) -> list[tuple[int,str]]:
+        # returns a sorted list of 2-tuples (or pairs) representing the ranked list of documents
+        pass
+
+    def and_query(self, query1:str, query2:str, k: int) -> list[tuple[int,str]]:
+        # returns the intersection of the ranked list of documents for query1 and query2
+        pass
+
+    def or_query(self, query1:str, query2:str, k: int) -> list[tuple[int,str]]:
+        # returns the union of the ranked list of documents for query1 and query2
+        pass
