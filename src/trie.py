a82b8d63c76bbaf6a16faedf873e6040b61462e5
ayilaemad <ae07352@st.habib.edu.pk>
2023-04-15 16:06:17 +0500

    Update trie.py

--- src/trie.py
+++ src/trie.py
@@ -1,5 +1,21 @@
 from document import Document
+
 class TrieNode:
-    pass
+
+    def __init__(self) -> None:
+        self.children = {}
+        self.end = False
+        pass
+
 class Trie:
-    pass
+
+    def __init__(self) -> None:
+        self.root = TrieNode()
+
+    def insert(self,word):
+        pass
+
+    def prefix_complete(self, prefix:str, node:TrieNode = None, word: str = "") -> list[tuple[int,int,str]]:
+        # returns a dict in which each key is a completion from the corpus and the corresponding value is a list of 3-tuples representing ID, start and end index
+        
+        pass 
