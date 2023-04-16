b287dca101299e67b19fb309e5955ad2dcec717d
Rohan Raj <rohan.raaj2@gmail.com>
2023-04-16 18:20:45 +0500

    debugging part 2

--- src/index.py
+++ src/index.py
@@ -3,32 +3,37 @@ from document import Document
 
 class InvertedIndex:
 
-    def __init__(self, Any: list) -> None:
-        self.list = Any
-        # print (Any)
-        
+    def __init__(self, docs: list) -> None:
+        self.list_of_docs = docs
 
     def query(self, terms:str, k: int) -> list[tuple[int,str]]:
         # returns a sorted list of 2-tuples (or pairs) representing the ranked list of documents
         list_of_words = terms.split()
         number_of_words = len(list_of_words)
+
+        # x = self.list.__contains__
         c = 0
         document_list = []
-        for i in self.list:
+        documents_dictionary = {}
+        for i in self.list_of_docs:
             c += 1
-            print ("Document:", c)
+            # print ("Document:", c)
+            t = 0
             for j in i:
                 if j == list_of_words[0]:
-                    document_list.append(("Document: ", c))
-                    print(j, "(found)")
+                    t += 1
+                    document_list.append("Doc" + str(c))
+                    documents_dictionary["Doc" + str(c)] = t
+                    # print(j, "(found)")
                     # print()
-            print ("document end")
+            # print ("document end")
         print (document_list)
+        print (documents_dictionary)
         # for word_number in range(len(list_of_words)):
         # x = [([k, terms])]
         # x.sort()
         # print (x)
-        # return x
+        return document_list
 
 
     def and_query(self, query1:str, query2:str, k: int) -> list[tuple[int,str]]:
