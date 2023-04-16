57de5f6345706fa04395487bca2abcd7c52220eb
Rohan Raj <rohan.raaj2@gmail.com>
2023-04-16 16:09:44 +0500

    inverted argument error fixed

--- src/index.py
+++ src/index.py
@@ -2,6 +2,10 @@ import math
 from document import Document
 
 class InvertedIndex:
+
+    def __init__(self, Any: list) -> None:
+        self.list = Any
+
     def query(self, terms:str, k: int) -> list[tuple[int,str]]:
         # returns a sorted list of 2-tuples (or pairs) representing the ranked list of documents
         pass
