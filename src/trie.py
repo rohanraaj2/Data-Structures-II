f6d304f3a9094e7a63e5f06c6692d5b19069cec9
ayilaemad <ae07352@st.habib.edu.pk>
2023-04-16 15:59:57 +0500

    Update trie.py

--- src/trie.py
+++ src/trie.py
@@ -16,8 +16,23 @@ class Trie:
 
     def prefix_complete(self, prefix:str, node:TrieNode = None, word: str = "") -> dict[str,list[tuple[str,int,int]]]:
         # returns a dict in which each key is a completion from the corpus and the corresponding value is a list of 3-tuples representing ID, start and end index
+        node = self.root
+        words = {}
+        for val in prefix: 
+            if val in node.children: 
+                node = node.children[val] 
+            else:
+                return words
+            word += val
         
-        pass 
+        if node.end:
+            words[word] = node.locations
+
+        for child, next_child in node.children.items():
+            child_completions = self.prefix_complete("", next_child, word + child)
+            words.update(child_completions)
+    
+        return words
 
     # helper functions
 
@@ -35,7 +50,7 @@ class Trie:
                 end = location[i][1]
                 node.locations.append((doc, start, end))
 
-    def _insert_doc(self, doc : Document):
+    def add_document(self, doc : Document):
         # inserts a new doc in the trie
         # inserting a new doc means inserting each word from the doc 
         for word in doc:
